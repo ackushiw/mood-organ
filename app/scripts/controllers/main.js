5c95f7a2be6dc39bced2df0ae1d5c1f19c8307f2
ackushiw <ackushiw@users.noreply.github.com>
2014-02-23 20:55:25 -0500

    fixed bad merge

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,28 +1,6 @@
 'use strict';
 
-
-<<<<<<< HEAD
-moodApp.controller('MainCtrl', function($scope) {
-          $scope.post = {url: 'http://', title: ''};
-        });
-=======
 angular.module('moodOrganApp')
-<<<<<<< HEAD
-  .controller('MainCtrl', function ($scope) {
-    $scope.mood = {
-      author: '',
-      decription: '',
-      id: '',
-      rating: 0,
-      shared: 0,
-      tag: '#',
-      time: '',
-      title: ''
-    };
-    $scope.post = {url: 'http://', title: ''};
-	});
->>>>>>> e7fe0e89bfd60188c3ea98e0e73529ab6a387b0e
-=======
 .controller('MainCtrl', function ($scope, syncData, moodPath) {
   $scope.emptyMood = {
     author: '',
@@ -47,4 +25,4 @@ angular.module('moodOrganApp')
     $scope.mood = $scope.emptyMood;
   };
 });
->>>>>>> ec4867b4e3d6c831c48010b5f1e0461e8d5f5cb9
+
