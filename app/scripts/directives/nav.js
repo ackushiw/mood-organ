a760ef4ce3f58aa60414305e9001c0749842d17e
ackushiw <ackushiw@users.noreply.github.com>
2014-02-26 01:31:30 -0500

    changed layout and added html

---
+++ app/scripts/directives/nav.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('moodOrganApp')
+  .directive('moodNav', function () {
+    return {
+      templateUrl: 'nav.html',
+      restrict: 'C',
+      
+      }
+    };
+  });
