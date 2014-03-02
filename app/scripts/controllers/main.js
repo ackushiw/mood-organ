8f41e1dda79422ea1bd5d49be0493650a94c2cd9
Peter Willis <peter@sycora.com>
2014-03-02 10:22:57 +0000

    Re-design main view
    Use three lists in main.js for search, shared and rated
    re-indent nav.js
    remove un necessary styles from main.css
    remove un necessary ng-controller attribute from login-modal.html
    total relayout of main.html
    relayout of mood-creation.html
    relayout of nav.html

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,39 +3,39 @@
 angular.module('moodOrganApp')
 .controller('MainCtrl', function ($scope, syncData, moodPath) {
   $scope.emptyMood = {
-    author: '',
+    created: '',
     decription: '',
+    from: '',
     id: '',
     rating: 0,
     shared: 0,
     tag: '#',
-    time: '',
     title: ''
   };
-  //syncData is defined in services/firebase.js, it is the easiest way to get at the data. moodPath is a constant defined in angularfire/config.js
-  $scope.moodlist = syncData(moodPath, 10);
-
+  //the search mood list should be the most recent, incorporating whatever is put in the search box
+  // this will eventually be a search query to elastic search
+  $scope.searchMoodlist = syncData(moodPath, 10);
   
-    $scope.addMood = function() {
-    	//changed from push to add as came up with errors
-        $scope.moodlist.$add({
-        	//test fields
-            //from: $scope.auth.user.username,
-            //id: $scope.idnumber,
-            //body: $scope.mood
-
-            //final fields
-            from: $scope.auth.user.username,
-            decription: $scope.mood,
-            id: $scope.idnumber,
-            rating: 0,
-            shared: 0,
-            tag: $scope.hash,
-            //this would be the timestamp of the add to firebase
-            time: new Date(),
-            title: $scope.mood
-        });
-        $scope.mood = $scope.emptyMood;
+  // this will be a search for the top shared moods over the last week/day/hour whatever (number of shares within a particular timeframe)
+  // this is also possible in elastic search
+  $scope.sharedMoodlist = syncData(moodPath, 1);
+  
+  // again this will be something similar, the highest rated mood in the recent history
+  $scope.ratedMoodlist = syncData(moodPath, 1);
+  
+  $scope.addMood = function() {
+    //changed from push to add as came up with errors
+    $scope.searchMoodlist.$add({
+      created: new Date(),
+      decription: $scope.mood,
+      from: $scope.auth.user.username,
+      id: $scope.idnumber,
+      rating: 0,
+      shared: 0,
+      tag: $scope.hash,
+      title: $scope.mood
+    });
+    $scope.mood = $scope.emptyMood;
   };
 });
 
